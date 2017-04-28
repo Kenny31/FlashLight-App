0a2eca36ea8f54915f5be58a0e7103f336784f68
Karan <karanbatra31@gmail.com>
2017-04-28 17:52:04 +0530

    second commit

--- app/src/main/java/com/example/android/flash/MainActivity.java
+++ app/src/main/java/com/example/android/flash/MainActivity.java
@@ -9,6 +9,7 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.BoolRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +24,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -43,12 +46,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import static com.example.android.flash.R.id.fab;
+
 public class MainActivity extends AppCompatActivity {
 
     private CameraManager mCameraManager;
     private String mCameraId;
     private ImageButton mTorchOnOffButton;
     private Boolean isTorchOn;
+    private Boolean isFlickering;
+    private SeekBar freqSeekBar;
+    private int frequency;
+    private ImageButton mCameraButton;
+    private RelativeLayout mainLayout;
+    private Thread a = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +69,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        freqSeekBar = (SeekBar) findViewById(R.id.seek_bar);
         mTorchOnOffButton = (ImageButton) findViewById(R.id.torch_button);
+        mCameraButton = (ImageButton) findViewById(R.id.camera_button);
+        mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
         isTorchOn = false;
+        isFlickering = false;
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -106,9 +123,17 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     if (isTorchOn) {
                         turnOffFlashLight();
+                        mCameraButton.setVisibility(View.INVISIBLE);
+                        freqSeekBar.setVisibility(View.INVISIBLE);
+                        mTorchOnOffButton.setImageResource(R.drawable.off);
                         isTorchOn = false;
+                        isFlickering = false;
                     } else {
                         turnOnFlashLight();
+                        mCameraButton.setVisibility(View.VISIBLE);
+                        freqSeekBar.setVisibility(View.VISIBLE);
+                        mTorchOnOffButton.setImageResource(R.drawable.on);
+                        freqSeekBar.setProgress(0);
                         isTorchOn = true;
                     }
                 } catch (Exception e) {
@@ -116,38 +141,59 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        freqSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                frequency = freqSeekBar.getProgress();
+                isFlickering = false;
+                if(frequency > 0) {
+                    try {
+                        flash_effect(1000 - (100 * (frequency - 1)));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    isFlickering = false;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void turnOnFlashLight() {
-
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 mCameraManager.setTorchMode(mCameraId, true);
-
-                mTorchOnOffButton.setImageResource(R.drawable.on);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void turnOffFlashLight() {
 
+    public void turnOffFlashLight() {
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 mCameraManager.setTorchMode(mCameraId, false);
-
-                mTorchOnOffButton.setImageResource(R.drawable.off);
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     @Override
     protected void onStop() {
         super.onStop();
-        if(isTorchOn){
+        if (isTorchOn) {
             turnOffFlashLight();
         }
     }
@@ -155,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if(isTorchOn){
+        if (isTorchOn) {
             turnOffFlashLight();
         }
     }
@@ -163,9 +209,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(isTorchOn){
+        if (isTorchOn) {
             turnOnFlashLight();
         }
+    }
+
+    public void flash_effect(final int frequency) throws InterruptedException {
+        Log.d("FREQ", String.valueOf(frequency));
+
+        a = new Thread() {
+            public void run() {
+                Boolean isOn = false;
+                while (isFlickering){
+                    if(isOn) {
+                        turnOffFlashLight();
+                    } else {
+                        turnOnFlashLight();
+                    }
+                    isOn = !isOn;
+                    try {
+                        Thread.sleep(frequency);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(!isFlickering) {
+                    interrupt();
+                }
+            }
+        };
+
+        isFlickering = true;
+        a.start();
     }
 }
 
